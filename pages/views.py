93d186e4fc18ed462858a12e5b580ac34619fccf
Shehabul-Islam-Sawraz <1805088@ugrad.cse.buet.ac.bd>
2021-12-10 12:28:49 +0600

    About, services & contact pages configured

--- pages/views.py
+++ pages/views.py
@@ -5,3 +5,12 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'pages/home.html')
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+def services(request):
+    return render(request, 'pages/services.html')
+
+def contact(request):
+    return render(request,'pages/contact.html')

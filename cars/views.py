0e69985c9331771e3b43b1343a6837a7996ceffa
Shehabul-Islam-Sawraz <1805088@ugrad.cse.buet.ac.bd>
2021-12-23 23:27:37 +0600

    Dedicated Car's page designed and Pagination added

--- cars/views.py
+++ cars/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import get_object_or_404, render
 from .models import Car
+from django.core.paginator import EmptyPage,Paginator,PageNotAnInteger
 
 # Create your views here.
 
 def cars(request):
-    return render(request, 'cars/cars.html')
+    cars = Car.objects.all().order_by('-created_date')
+    paginator = Paginator(cars,4)
+    page = request.GET.get('page')
+    paged_cars = paginator.get_page(page)
+    data={
+        'cars': paged_cars,
+    }
+    return render(request, 'cars/cars.html', data)
 
 def car_detail(request, id):
     single_car = get_object_or_404(Car, pk=id)
